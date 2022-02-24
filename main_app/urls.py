8be641d917754efe9d42a44b4512c9d4fd55d3b2
aftab590 <2019aaftab.shaikh@ves.ac.in>
2022-02-24 11:56:59 +0530

    Apps added, front end connected with backend

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('admin_ui', views.admin_ui , name='admin_ui'),
     path('patient_ui', views.patient_ui , name='patient_ui'),
     path('doctor_ui', views.doctor_ui , name='doctor_ui'),
+    path('pviewprofile/<str:patientusername>', views.pviewprofile , name='pviewprofile'),
+    path('pconsultation_history', views.pconsultation_history , name='pconsultation_history'),
 ]
